9375e73e7e0480c79bff1c077417659950959454
Charles Fulton <fultonc@lafayette.edu>
2017-01-24 11:28:52 -0500

    Update front matter

--- settings.php
+++ settings.php
@@ -15,9 +15,9 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Local course merge settings definitions.
+ * Local course template settings definitions.
  *
- * @package   local_course_merge
+ * @package   local_course_template
  * @copyright 2016 Lafayette College ITS
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
